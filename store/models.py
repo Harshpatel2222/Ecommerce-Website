fa8adda031af4a0ed687ede7318f972a84d8fbe1
Harsh Patel <61933209+Harshpatel2222@users.noreply.github.com>
2021-05-21 13:12:21 +0530

    created database

--- store/models.py
+++ store/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.base import Model
+from django.db.models.deletion import SET_NULL
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, null=True)
+    email = models.CharField(max_length=150, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=150, null=True)
+
+    def __str__(self):
+        return str(self.transaction_id) 
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quatity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class ShippingAdress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    adress = models.CharField(max_length=150, null=True)
+    city = models.CharField(max_length=150, null=True)
+    state = models.CharField(max_length=150, null=True)
+    zip_code = models.CharField(max_length=150, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.adress
