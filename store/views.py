558d9938cb6d3e0ea3d0db7a27e2bc345307736b
Harsh Patel <61933209+Harshpatel2222@users.noreply.github.com>
2021-05-22 20:19:55 +0530

    csrf token added

--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+
 from .models import *
 
 # Create your views here.
@@ -9,11 +12,44 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {
+            'get_cart_total' : 0,
+            'get_cart_items' : 0
+        }
+    context={
+        'items' : items,
+        'order': order,
+        }
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {
+            'get_cart_total' : 0,
+            'get_cart_items' : 0
+        }
+    context={
+        'items': items,
+        'order' : order,
+    }
     return render(request, "store/checkout.html", context)
 
-    
+def updateItem(request):
+    # data =json.loads(request.data)
+    # productId = data['productId']
+    # action = data['action']
+
+    # print('Action: ',action)
+    # print('productId: ',productId)
+    return JsonResponse('Item Added', safe=False)
