46f286f5284546b687d1e65711c928a1199dcb75
Harsh Patel <61933209+Harshpatel2222@users.noreply.github.com>
2021-05-22 22:29:58 +0530

    update item or add item

--- store/models.py
+++ store/models.py
@@ -45,6 +45,7 @@ class Order(models.Model):
         total = sum([item.get_total for item in orderitems])
         return total
 
+    @property
     def get_cart_items(self):
         orderitems = self.orderitem_set.all()
         total = sum([item.quatity for item in orderitems])
--- store/views.py
+++ store/views.py
@@ -46,10 +46,25 @@ def checkout(request):
     return render(request, "store/checkout.html", context)
 
 def updateItem(request):
-    # data =json.loads(request.data)
-    # productId = data['productId']
-    # action = data['action']
+    data =json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action: ',action)
+    print('productId: ',productId)
 
-    # print('Action: ',action)
-    # print('productId: ',productId)
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quatity = (orderItem.quatity + 1)
+    elif action == 'remove':
+        orderItem.quatity = (orderItem.quatity - 1)
+
+    orderItem.save()
+
+    if orderItem.quatity <=0:
+        orderItem.delete
     return JsonResponse('Item Added', safe=False)
